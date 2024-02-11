de716ec6abdb3770a1c2f9e124d98333b9cdaabe
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2024-02-11 14:41:26 +0100

    Update Ogawa2001.lean

--- Ogawa2001.lean
+++ Ogawa2001.lean
@@ -1,3 +1,11 @@
 -- This module serves as the root of the `Ogawa2001` library.
 -- Import modules here that should be built as part of the library.
-import «Ogawa2001».Basic
+import «Ogawa2001».Test
+import «Ogawa2001».CleverTest
+import «Ogawa2001».ErrorOfTest
+import «Ogawa2001».SmallErrorOfCleverTest
+import «Ogawa2001».Util
+import «Ogawa2001».Stein
+import «Ogawa2001».State
+import «Ogawa2001».Pinch
+import «Ogawa2001».ExistsCleverTest
