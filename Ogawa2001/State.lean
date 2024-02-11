b107f82097427fb410e3532d195c4dd06f363024
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2024-02-11 14:28:21 +0100

    Update State.lean

--- Ogawa2001/State.lean
+++ Ogawa2001/State.lean
@@ -1,19 +1,40 @@
--- import data.complex.basic
--- import analysis.inner_product_space.basic
--- import linear_algebra.tensor_power
--- import order.liminf_limsup
--- import analysis.special_functions.complex.log
-
--- open real filter
-
--- open_locale tensor_product big_operators
+-- import Ogawa2001.Util
 
 
+-- open BigOperators NormedSpace Complex TensorProduct TensorPower -- Filter
 
--- class state {n : ℕ} (ρ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n :=
+
+-- variable {V : Type*} [NormedAddCommGroup V] [InnerProductSpace ℂ V] [FiniteDimensional ℂ V]
+
+
+-- class State (ρ : V →ₗ[ℂ] V) :=
 -- (trace_one : Tr ρ = 1)
--- (pos_semidef : pos_semidef ρ)
+-- (nonneg : 0 ≤ ρ)
 
 
 /- Quantum relative entropy: "distance" between state ρ and state σ -/
 -- def D(ρ||σ) := Tr[ρ (log ρ - log σ)]
+
+
+-- variable {W : Type*} [NormedAddCommGroup W] [InnerProductSpace ℂ W] [FiniteDimensional ℂ W] {ρ : W →ₗ[ℂ] W}
+
+-- #check W
+-- #check W →ₗ[ℂ] W
+-- #check ρ
+-- #check State
+-- #check State ρ
+
+
+-- instance : NormedAddCommGroup (TensorPower ℂ n W) := by sorry
+-- instance : InnerProductSpace ℂ (TensorPower ℂ n W) := by sorry
+-- instance : FiniteDimensional ℂ (TensorPower ℂ n W) := by sorry
+
+
+-- variable {n : ℕ} {ρₙ : (TensorPower ℂ n W) →ₗ[ℂ] (TensorPower ℂ n W)}
+
+
+-- #check (TensorPower ℂ n W)
+-- #check (TensorPower ℂ n W) →ₗ[ℂ] (TensorPower ℂ n W)
+-- #check ρₙ
+-- #check State
+-- #check State ρₙ
